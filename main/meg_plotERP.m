e7ba821be1bc933af81ceb8510c9f4209e852d7f
karentian <9136901+karentian@users.noreply.github.com>
2020-01-12 10:56:23 -0500

    update plotERP checks, errors, plot of avg all selectedChannels

--- main/meg_plotERP.m
+++ main/meg_plotERP.m
@@ -1,117 +1,139 @@
-function meg_plotERP(data,selectedChannels,plotSingleTrial,plotAvgTrial)
+function fH = meg_plotERP(data,selectedChannels,plotSingleTrial,plotAvgTrial,plotAvgChannel)
 
 % MEG_PLOTERP(data,selectedChannels,plotSingleTrial,plotAvgTrial)
-% 
-% data 
+%
+% data
 %   structure of condition cells containing
 %       data matrix, time x channels x trials
-% selectedChannels 
+% selectedChannels
 %   vector, default 1:157 meg channels
-% plotSingleTrial
+% plotSingleTrial by channel by condition
 %   1 or 0, default 1
 % plotAvgTrial
 %   1 or 0, default 1
-% figDir 
-%   path to figure directory 
+% plotAvgChannel
+%   1 or 0, default 1
 %
 % Karen Tian
-% January 2020 
+% January 2020
 
-%% args 
+%% args
 
-% if nargin<5
-%     figDir = '/Users/kantian/Dropbox/CarrascoLab/TA2Figs';
-% end
-if nargin<4 % default plots avg trial 
-    plotAvgTrial = 1; 
+if nargin<5
+    plotAvgChannel = 1;
 end
-if nargin<3 % default plots single trial 
-    plotSingleTrial = 1;  
+if nargin<4 % default plots avg trial
+    plotAvgTrial = 1;
 end
-if nargin<2 % default selects channels 1:3 
-    selectedChannels = 1:3;    
+if nargin<3 % default plots single trial
+    plotSingleTrial = 1;
+end
+if nargin<2 % default selects channels 1:3
+    selectedChannels = 1:3;
+    disp('selectedChannels not specified, default 1:3')
 end
 if nargin<1
-    load('data.mat'); % load dummy data 
+    load('data.mat'); % load dummy data
 end
 
+%% setup
 saveFigs = 0;
- 
-fieldName = fieldnames(data);
 
-eventTimes = [0 1000 1250 2100]; 
+fieldName = fieldnames(data);
+nFields = numel(fieldName);
+nChannels = numel(selectedChannels);
+
+eventTimes = [0 1000 1250 2100]+500; % check timing
 eventNames = {'precue','T1','T2','response cue'};
 
-%% plot ERP single trial by channel
+t = 1:size(data.(fieldName{1}),1);
+xlims = [min(t),max(t)];
+
+%% checks
+
+% check data
+sz = size(data.(fieldName{1}));
+if numel(sz)~=3
+    error('data is expected to be 3-dimensional, with trials as the last dimension')
+end
+    
+% check channels 
+if ~isnumeric(selectedChannels)
+    error('selectedChannels is expected to be a vector of channel numbers')
+end
+
+%% plots ERP: fig per channel, subplot per condition, single trials 
 
 if plotSingleTrial
     
-    for iF=1:numel(fieldName)
+    for iC = selectedChannels
         figure
-        dataField = data.(fieldName{iF}); 
-        t = 1:size(dataField,1); 
-        xlims = [min(t),max(t)]; 
-        nPlot = 1; 
-        if(isnumeric(dataField))
-            for iC = selectedChannels
-                subplot (length(selectedChannels),1,nPlot)
-                hold on
-                plot(t, squeeze(dataField(:,iC,:)))
-                title(sprintf('%s channel %d',fieldName{iF},iC))
-                xlim(xlims)
-                vline(eventTimes,'k',eventNames)
-                xlabel('time (ms)')
-                ylabel('amplitude')
-                nPlot = nPlot + 1; 
-            end
+        for iF=1:nFields
+            vals = data.(fieldName{iF});
+            subplot (nFields,1,iF)
+            hold on
+            plot(t, squeeze(vals(:,iC,:)))
+            title(sprintf('%s',fieldName{iF}))
+            xlim(xlims)
+            xlabel('time (ms)')
+            ylabel('amplitude')
+            vline(eventTimes,'k',eventNames)
         end
+        rd_supertitle2(sprintf(sprintf('channel %d',iC)))
     end
     
 end
 
-%% plot ERP average trial by channel 
+%% plot ERP average trial by channel
 
 if plotAvgTrial
     
-    for iF=1:numel(fieldName)
-        figure
-        dataField = data.(fieldName{iF}); 
-        t = 1:size(dataField,1); 
-        xlims = [min(t),max(t)]; 
-        nPlot = 1; 
-        if(isnumeric(dataField))
-            for iC = selectedChannels
-                subplot (length(selectedChannels),1,nPlot)
-                hold on
-                meanTrial = nanmean(dataField(:,iC,:),3);               
-                plot(t, squeeze(meanTrial))
-                title(sprintf('%s channel %d mean',fieldName{iF},iC))
-                xlim(xlims)
-                vline(eventTimes,'k',eventNames)
-                xlabel('time (ms)')
-                ylabel('amplitude')
-                nPlot = nPlot + 1; 
-            end
+    figure
+    for iC=1:nChannels
+        for iF=1:nFields
+            vals = data.(fieldName{iF});
+            meanTrial = nanmean(vals(:,iC,:),3);
+            meanChannel = nanmean(meanTrial,2);
+            subplot (nChannels,1,iC)
+            hold on
+            plot(t, meanChannel)
         end
+        xlim(xlims)
+        vline(eventTimes,'k',eventNames)
+        xlabel('time (ms)')
+        ylabel('amplitude')
+        title(sprintf('channel %d',iC))
     end
+    legend(fieldName)
+    rd_supertitle2('avg ERP')
     
 end
 
-%% save figs
 
-if saveFigs
-    if ~exist(figDir,'dir')
-        mkdir(figDir)
+%% plot ERP average channel
+
+if plotAvgChannel
+    
+    figure
+    for iF=1:nFields
+        vals = data.(fieldName{iF});
+        meanTrial = nanmean(vals(:,selectedChannels,:),3);
+        meanChannel = nanmean(meanTrial,2);
+        hold on
+        plot(t, meanChannel)  
     end
-    f = sort(findobj('Type','figure'));
-    for iF = 1:numel(f)
-        if isnumeric(f)
-            figNames{iF} = num2str(f(iF));
-        else
-            figNames{iF} = num2str(f(iF).Number);
-        end
-    end
-    rd_saveAllFigs(f,figNames,[],figDir);
+    xlim(xlims)
+    vline(eventTimes,'k',eventNames)
+    xlabel('time (ms)')
+    ylabel('amplitude')
+    title(sprintf('channel %d ',selectedChannels))
+    legend(fieldName)
+    rd_supertitle2('avg ERP')
+    
 end
+
+
+%% return figure handle
+
 
 
