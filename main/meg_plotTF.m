8ed04cfbe79e21b0cf1b408fa53bae05c1d9f3e3
karentian <9136901+karentian@users.noreply.github.com>
2020-01-17 11:33:28 -0500

    update meg_plotTF.m freq range

--- main/meg_plotTF.m
+++ main/meg_plotTF.m
@@ -1,4 +1,4 @@
-function [tfAmps,freqoi,timeoi,fH] = meg_plotTF(data,selectedChannels,selectedFreq)
+function [A,fH] = meg_plotTF(data,selectedChannels,selectedFreq)
 
 % MEG_PLOTERP(data,selectedChannels,plotSingleTrial,plotAvgTrial)
 %
@@ -12,12 +12,12 @@ function [tfAmps,freqoi,timeoi,fH] = meg_plotTF(data,selectedChannels,selectedFr
 %% args
 
 if nargin<3
-    selectedFreq = 8;
-    disp('selectedChannels not specified, default 8')
+    selectedFreq = 8:14;
+    disp('selectedFreqs not specified, default 8-14 Hz (alpha band, Foxe 2011')
 end
 if nargin<2 % default selects channels 1:3
-    selectedChannels = 1:3;
-    disp('selectedChannels not specified, default 1:3')
+    selectedChannels = [20,23,36,43,60];
+    disp('selectedChannels not specified, default [20,23,36,43,60]')
 end
 if nargin<1
     load('data.mat'); % load dummy data
@@ -29,6 +29,7 @@ saveFigs = 0;
 condNames = fieldnames(data);
 nConds = numel(condNames);
 nChannels = numel(selectedChannels);
+nFreqs = numel(selectedFreq); 
 
 %% checks
 
@@ -50,7 +51,7 @@ eventTimes = [0 1000 1250 2100]; % check timing
 eventNames = {'precue','T1','T2','response cue'};
 tstart = -500; % -1000; 
 tstop = 2800; % 2300;
-t = tstart:tstop;
+t = tstart:10:tstop;
 
 taper          = 'hanning';
 foi            = 1:100;
@@ -61,6 +62,8 @@ tfAmps = [];
 % figures
 ytick = 10:10:numel(foi);
 xtick = 1:50:numel(toi);
+xtickms = tstart:500:tstop;
+% label = {'-500' '0' '500' '1000' '1500' '2000' '2500'}; % {num2str(xtickms')}; 
 
 ylims = [min(foi),max(foi)]; 
 xlims = [size(toi,1),size(toi,2)]; 
@@ -76,26 +79,31 @@ for iC = 1:nConds
     cond = condNames{iC}; 
     % erf.(thisField) = squeeze(nanmean(data.(thisField)(:,selectedChannels,:),2)); % time x trial
     
-    y = fft(data.(cond)(:,selectedChannels,:),nfft)/nSamples; % freq x trial 
+    y = fft(data.(cond),nfft)/nSamples; % freq x trial 
     f = Fsample/2*linspace(0,1,nfft/2+1); % frequencies
-    amps = 2*abs(y(1:nfft/2+1,:)); % amp of freq x trial 
+    amps = 2*abs(y(1:nfft/2+1,:,:)); % amp of freq x trial 
     ampsMean = nanmean(amps,3);
     
     % fieldtrip input
-    vals = nanmean(data.(cond)(:,selectedChannels,:),3)'; % channels by samples
+    vals = nanmean(data.(cond)(:,:,:),3)'; % channels by samples
     [spectrum,ntaper,freqoi,timeoi] = ft_specest_mtmconvol(vals, t/1000, ...
         'timeoi', toi, 'freqoi', foi, 'timwin', t_ftimwin, ...
         'taper', taper, 'dimord', 'chan_time_freqtap');
     
-    specAmp = squeeze(mean(abs(spectrum),1)); % mean across channels
-    tfAmps = specAmp'; % foi x time
+    specAmp = squeeze(nanmean(abs(spectrum),1)); % mean across all channels
+    
+    specSelectedCh = spectrum(selectedChannels,:,:); 
+    specSelectChMean = squeeze(nanmean(abs(specSelectedCh),1)); 
+    
+    tfAmps = specSelectChMean'; % foi x time
     
     % store variables
     A.(cond).y = y; 
     A.(cond).f = f; 
     A.(cond).amps = amps; 
     A.(cond).ampsMean = ampsMean; 
-    A.(cond).specAmp = specAmp;
+    A.(cond).specSelectedCh = specSelectedCh;
+    A.(cond).specSelectChMean = specSelectChMean;
     A.(cond).tfAmps = tfAmps;
     
 end
@@ -103,20 +111,22 @@ end
 %% spectrogram plot 
 
 figure
-set(gcf, 'Position',  [100, 100, 1000, 300*ceil(nConds/4)])
 for iC = 1:nConds
     cond = condNames{iC};
     if nConds <= 4
         subplot (1,nConds,iC)
+        set(gcf, 'Position',  [100, 100, 300*nConds, 300*ceil(nConds/4)])
     end
     if nConds > 4
         subplot (ceil(nConds/3),nConds,iC)
+        set(gcf, 'Position',  [100, 100, 300*ceil(nConds/4), 300*ceil(nConds/4)])
     end
     hold on
     imagesc(A.(cond).tfAmps)
     xlim(xlims)
     ylim(ylims)
-    xlabel('time (ms)')
+    xticklabels(num2str(xtickms))
+    xlabel('time (s)')
     ylabel('frequency (Hz)')
     colorbar
     caxis(clims)
@@ -127,13 +137,15 @@ end
 %% normalized spectrogram plot 
 
 figure
-set(gcf, 'Position',  [100, 100, 1000, 300*ceil(nConds/4)])
+set(gcf, 'Position',  [100, 100, 300*ceil(nConds/4), 300*ceil(nConds/4)])
 for iC = 1:nConds
     cond = condNames{iC};
     if nConds <= 4
+        set(gcf, 'Position',  [100, 100, 300*nConds, 300*ceil(nConds/4)])
         subplot (1,nConds,iC)
     end
     if nConds > 4
+        set(gcf, 'Position',  [100, 100, 300*ceil(nConds/4), 300*ceil(nConds/4)])
         subplot (ceil(nConds/3),nConds,iC)
     end
     hold on
@@ -146,12 +158,13 @@ for iC = 1:nConds
     imagesc(normPow)
     xlim(xlims)
     ylim(ylims)
-    xlabel('time (ms)')
+    xlabel('time (s)')
     ylabel('frequency (Hz)')
     colorbar
     % caxis(clims)
+    xticklabels(num2str(xtickms))
     meg_timeFreqPlotLabels(toi,foi,xtick,ytick,eventTimes);
-    title(sprintf('%s \n channels %s ',und2space(cond), num2str(selectedChannels)))
+    title(sprintf('normalized %s \n channels %s ',und2space(cond), num2str(selectedChannels)))
 end
 
 %% fourier plot
@@ -171,24 +184,37 @@ for iC = 1:nConds
     ylabel('amp')
 end
 
-%% plot selected frequencies for all conditions
+%% fourier plot average
 
 figure
-for iH = 1:numel(selectedFreq)
+set(gcf, 'Position',  [100, 100, 800, 300])
+for iC = 1:nConds
+    cond = condNames{iC};
+    selectChAmps = nanmean(A.(cond).ampsMean(:,selectedChannels),2); 
+    loglog(A.(cond).f, selectChAmps) 
     hold on
-    freq = selectedFreq(iH);
-    for iC = 1:nConds
-        cond = condNames{iC};
-        fVals = squeeze(A.(cond).tfAmps(freq,:));
-        plot(toi*1000, fVals)
-    end
-    % xlim([t(1),t(end)])
-    ylabel('amp')
-    xlabel('time (ms)')
-    vline(eventTimes,'k',eventNames)
+    xlim([A.(cond).f(1) A.(cond).f(end)])
+    title('fft')
     legend(condNames)
-    title(sprintf('Frequency %d (Hz)',freq))
+    xlabel('frequency (Hz)')
+    ylabel('amp')
 end
+
+%% plot selected frequency range for all conditions
+
+figure
+hold on
+for iC = 1:nConds
+    cond = condNames{iC};
+    fVals = squeeze(nanmean(A.(cond).tfAmps(selectedFreq,:),1));
+    plot(t, fVals)
+end
+xlim([t(1),t(end)])
+ylabel('amp')
+xlabel('time (ms)')
+vline(eventTimes,'k',eventNames)
+legend(condNames)
+title(sprintf('Frequency %d - %d (Hz)',selectedFreq(1),selectedFreq(end)))
 
 %% return figure handle
 
