29ec02c22c12e34fef02a15d8e54cc1f8ec7e3d4
karentian <9136901+karentian@users.noreply.github.com>
2020-01-20 21:33:41 -0500

    create meg_params.m

---
+++ io/meg_params.m
@@ -0,0 +1,23 @@
+function p = meg_params(expt)
+
+% function p = meg_params(expt)
+%
+% INPUT
+%   expt: string expt type ('TA2') 
+% OUTPUT
+% p
+%   parameters
+%
+% Karen Tian
+% January 2020
+
+switch expt
+    case 'TA2'
+        % timing 
+        p.eventTimes = [0 1050 1300 2100]; % accounting for 50ms photodiode delay
+        p.eventNames = {'precue','T1','T2','response cue'};
+        p.tstart = -500; % -1000; 
+        p.tstop = 2800; % 2300;   
+    otherwise 
+        disp('expt type not recognized')
+end
