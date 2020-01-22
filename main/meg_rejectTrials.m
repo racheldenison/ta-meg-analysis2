721134f2106c25cfaeac4b10d9409c7bf7b7927a
karentian <9136901+karentian@users.noreply.github.com>
2020-01-22 14:54:19 -0500

    create meg_rejectTrials.m

---
+++ main/meg_rejectTrials.m
@@ -0,0 +1,21 @@
+function data = meg_rejectTrials(sessionDir,data)
+
+% data = MEG_REJECTTRIALS(data)
+%
+% INPUT
+% data
+%   data matrix, time x channels x trials
+% OUPUT 
+% data
+%   data matrix, time x channels x trials rejected NaN
+%
+% Karen Tian
+% January 2020
+
+%% NaN manually rejected channels 
+
+load(sprintf('/Users/kantian/Dropbox/Data/TA2/MEG/%s/prep/trials_rejected.mat',sessionDir)); 
+data(:,:,trials_rejected) = NaN; 
+
+end
+
