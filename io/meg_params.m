37e8d52111665d270765da996791e456e5360add
karentian <9136901+karentian@users.noreply.github.com>
2020-02-21 15:03:41 -0500

    update meg_params.m timing

--- io/meg_params.m
+++ io/meg_params.m
@@ -3,7 +3,7 @@ function p = meg_params(expt)
 % function p = meg_params(expt)
 %
 % INPUT
-%   expt: string expt type ('TA2') 
+%   expt: string expt type ('TA2_Preproc','TA2_Analysis','TANoise_Preproc','TANoise_Analysis') 
 % OUTPUT
 % p
 %   parameters, including timing, channel info
@@ -12,27 +12,42 @@ function p = meg_params(expt)
 % January 2020
 
 p.fSample = 1000; % frame rate, Hz
-p.megChannels = 1:157; % 1 indexed
+p.megChannels = 1:157; % 
+p.eventNames = {'precue','T1','T2','response cue'};
 
 switch expt
     
-    case 'TA2'
+    case 'TA2_Preproc'
         % timing 
-        p.eventTimes = [0 1050 1300 2100]; % accounting for 50ms photodiode delay
-        p.eventNames = {'precue','T1','T2','response cue'};
-        p.tstart = -500; % -1000; 
-        p.tstop = 2800; % 2300;   
-        p.prestim = 0.5; 
-        p.poststim = 2.8; 
-   
-    case 'TANoise'
-        % add timing
-        p.eventTimes = [0 1000 1250 2100];
-        p.eventNames = {'precue','T1','T2','response cue'};
+        p.eventTimes = [0 1050 1350 2300]; % accounting for 50ms photodiode delay
+        p.tstart = -200; % -1000; 
+        p.tstop = 2300; % 2300;   
+        p.prestim = 0.2; 
+        p.poststim = 2.3; 
+        
+    case 'TA2_Analysis'
+        % timing 
+        p.eventTimes = [0 1050 1350 2300]; % accounting for 50ms photodiode delay
         p.tstart = -500; 
-        p.tstop = 3100; 
+        p.tstop = 3900; 
         p.prestim = 0.5; 
-        p.poststim = 3.1; 
+        p.poststim = 3.9; 
+   
+    case 'TANoise_Preproc'
+        % add timing
+        p.eventTimes = [0 1000 1250 2300];
+        p.tstart = -200; 
+        p.tstop = 2300; 
+        p.prestim = 0.2; 
+        p.poststim = 2.3; 
+        
+    case 'TANoise_Analysis'
+        % add timing
+        p.eventTimes = [0 1000 1250 2300];
+        p.tstart = -500; 
+        p.tstop = 3900; 
+        p.prestim = 0.5; 
+        p.poststim = 3.9; 
     otherwise 
         disp('expt type not recognized')
 end
