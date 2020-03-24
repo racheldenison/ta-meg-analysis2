435e7c49af5646c9b0a08cc0a21edde9e316bcfe
Rachel Denison <r.n.denison@gmail.com>
2020-03-23 22:07:25 -0400

    separate function meg_plotDecode.m

--- main/meg_decode.m
+++ main/meg_decode.m
@@ -1,6 +1,6 @@
-function [A, fH] = meg_decode(data, classLabels, classNames, p)
+function A = meg_decode(data, classLabels, classNames, p)
 
-% function [A, fH] = meg_decode(data, classLabels, classNames, p)
+% function A = meg_decode(data, classLabels, classNames, p)
 %
 % INPUTS
 % data
@@ -23,9 +23,6 @@ function [A, fH] = meg_decode(data, classLabels, classNames, p)
 % OUTPUTS
 % A
 %   analysis structure
-%
-% fH
-%   figure handles
 % 
 % Rachel Denison
 % March 2020
@@ -57,11 +54,6 @@ if isfield(p,'channels')
 else
     channels = 1:size(data,2);
 end
-if isfield(p,'hdr')
-    data_hdr = p.data_hdr; % data header for plotting topologies
-else
-    load data/data_hdr.mat
-end
 
 %% Setup
 classes = unique(classLabels(~isnan(classLabels)));
@@ -76,6 +68,9 @@ if nC~=2
 end
 if numel(classes)~=nC
     error('Number of classes in classLabels does not match classNames')
+end
+if size(classLabels,1)==1
+    classLabels = classLabels'; % should be column vector
 end
 
 %% Organize data
@@ -196,130 +191,35 @@ for iRep = 1:nReps
             model = [];
         end
     end
-    toc
-    
-    classAcc(:,iC) = acc;
-    classModel{iC} = model;
-    
-    % trial average
-    classAccNT(:,:,iRep) = classAcc;
-    classModelNT(:,iRep) = classModel';
+
+    classAccNT(:,iC,iRep) = acc;
+    classModel{iC,iRep} = model;
 end
+
+% trial average
+classAcc = mean(classAccNT,3);
 
 %% extract channel weights 
 if getWeights
-    classWeights = [];
+    classWeightsNT = [];
     for iC = 1:nC/2
         for iTime = 1:numel(times)
             for iRep = 1:nReps
-                model = classModelNT{iC,iRep}(iTime);
+                model = classModel{iC,iRep}(iTime);
                 
                 w = model.SVs' * model.sv_coef;
                 b = -model.rho;
                 if (model.Label(1) == -1)
                     w = -w; b = -b;
                 end
-                classWeights(:,iTime,iC,iRep) = w;
+                classWeightsNT(:,iTime,iC,iRep) = w;
             end
         end
     end
+    classWeights = mean(classWeightsNT,4);
 else
     classWeights = [];
 end
-
-%% plot
-xlims = targetWindow;
-ylims = [30 100];
-
-fH(1) = figure;
-hold on
-plot(times, mean(classAccNT,3),'LineWidth',1)
-plot(times, mean(mean(classAccNT,3),2), 'k')
-plot(xlims,[50 50],'k')
-xlim(xlims)
-ylim(ylims)
-xlabel('Time (ms)')
-ylabel('Classification accuracy (%)')
-legend(classNames)
-
-% if saveFigs
-%     rd_saveAllFigs(gcf, {sprintf('%s_%s',figName{1},decodeAnalStr)}, 'plot', figDir)
-% end
-
-%% topo weights movie T1 and T2
-if getWeights
-    clims = [-2 2];
-    
-    fH(2) = figure('Position',[250 850 950 450]);
-    for iTime = 1:numel(times)
-        for iC = 1:nC/2
-            vals = squeeze(mean(classWeights(:,iTime,iC,:),4))';
-            subplot(1,nC/2,iC)
-            ssm_plotOnMesh(vals, '', [], data_hdr, '2d');
-            set(gca,'CLim',clims)
-            colorbar
-            title(classNames{iC})
-        end
-        rd_supertitle2(sprintf('t = %d', times(iTime)))
-        pause(0.2)
-    end
-end
-
-%% topo weights for specific time intervals
-twins = {[110 140], [140 230], [230 280], [280 315], [110 315]};
-
-if getWeights
-    clims = [0 1.5];
-
-    for iTW = 1:numel(twins)
-        twin = twins{iTW};
-        tidx = find(times==twin(1)):find(times==twin(2));
-        
-        figure('Position',[250 850 950 450]);
-        for iC = 1:nC/2
-            vals = squeeze(mean(mean(abs(classWeights(:,tidx,iC,:)),4),2))';
-            subplot(1,nC/2,iC)
-            ssm_plotOnMesh(vals, '', [], data_hdr, '2d');
-            set(gca,'CLim',clims)
-            colorbar
-            title(classNames{iC})
-        end
-        rd_supertitle2(sprintf('%d-%d ms', twin(1), twin(2)))
-        
-%         if saveFigs
-%             rd_saveAllFigs(gcf, ...
-%                 {sprintf('%s_%s_%d-%dms','svmWeights',decodeAnalStr, twin(1), twin(2))}, 'map', figDir)
-%         end
-    end
-end
-
-%% mean across longest interval, reps, and orientations
-twin = [110 315];
-tidx = find(times==twin(1)):find(times==twin(2));
-nTopChannels = 10;
-
-vals = squeeze(mean(mean(mean(abs(classWeights(:,tidx,:,:)),4),2),3))';
-[sortedVals, idx] = sort(vals,'descend');
-topChannels = idx(1:nTopChannels);
-
-fH(3) = figure('Position',[250 850 950 450]);
-subplot(1,3,1)
-histogram(vals)
-xlabel('Unsigned SVM weight')
-ylabel('Count')
-subplot(1,3,2)
-ssm_plotOnMesh(vals, '', [], data_hdr, '2d');
-title('Unsigned SVM weights')
-subplot(1,3,3)
-ssm_plotOnMesh(double(vals>=sortedVals(nTopChannels)), '', [], data_hdr, '2d');
-title(['Channels ' sprintf('%d ',topChannels)])
-rd_supertitle2(sprintf('%d-%d ms', twin(1), twin(2)))
-
-% if saveFigs
-%     rd_saveAllFigs(gcf, ...
-%         {sprintf('%s_%s_%d-%dms_top%dCh','svmWeights',decodeAnalStr, twin(1), twin(2), nTopChannels)},...
-%         'map', figDir)
-% end
 
 %% store results
 A.classNames = classNames;
@@ -330,6 +230,7 @@ A.decodingOps.binSize = sp;
 A.decodingOps.kfold = kfold;
 A.decodingOps.svmops = svmops;
 A.classTimes = times;
+A.classAccNT = classAccNT;
 A.classAcc = classAcc;
 A.classModel = classModel;
 A.classWeights = classWeights;
