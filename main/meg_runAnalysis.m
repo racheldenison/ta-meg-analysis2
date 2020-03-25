573916ad5a86d6d787dfe81196b749e2cc89bbbc
karen <karenjtian@gmail.com>
2020-03-25 11:05:29 +0800

    update meg_runAnalysis.m

--- main/meg_runAnalysis.m
+++ main/meg_runAnalysis.m
@@ -1,4 +1,4 @@
-function meg_runAnalysis(sessionDir)
+function [selectedChannels,D] = meg_runAnalysis(sessionDir)
 % 
 % setup 
 % read data
@@ -12,16 +12,29 @@ function meg_runAnalysis(sessionDir)
 % Karen Tian 
 % January 2020 
 
+% sessionDir = 'R0817_20181120';
+
 %% session info 
 
-sessionDir = 'R0817_20181120';
-exptShortName = 'TA2'; % TANoise
-analStr = 'bietfp';
+% TA2
+exptShortName = 'TA2'; 
 exptDir = '/Users/kantian/Dropbox/Data/TA2/MEG'; 
+p = meg_params('TA2_Analysis');
 
-p = meg_params('TA2');
-readData = 0; % need to read data first time
-loadData = 1; % reload data matrix 
+% TANoise
+% exptShortName = 'TANoise'; % TANoise
+% exptDir = '/Users/kantian/Dropbox/Data/TANoise/MEG'; 
+% p = meg_params('TANoise_Analysis');
+
+analStr = 'bietfp';
+readData = 0; % newly prepare and read data
+loadData = 1; % load already prepared data matrix 
+selectChannels = 0; 
+loadSelectedChannels = 1; 
+
+plotERF = 0; 
+plotTF = 0;  
+saveTF = 0; % save time frequency mat 
 
 %% setup 
 
@@ -30,6 +43,7 @@ fileBase = sessionDirToFileBase(sessionDir, exptShortName);
 dataDir = sprintf('%s/%s', exptDir, sessionDir);
 matDir = sprintf('%s/mat', dataDir);
 preprocDir = sprintf('%s/preproc', dataDir);
+prepDir = sprintf('%s/prep', dataDir);
 
 filename = sprintf('%s/%s_%s.sqd', preprocDir, fileBase, analStr); % *run file* 
 figDir = sprintf('%s/figures/%s', dataDir);
@@ -38,9 +52,9 @@ behavDir = sprintf('%s/Behavior/%s/analysis', exptDir(1:end-4), sessionDir);
 behavFile = dir(sprintf('%s/*.mat', behavDir));
 behav = load(sprintf('%s/%s', behavDir, behavFile.name));
 
-eyesClosedBase = sessionDirToFileBase(sessionDir, 'EyesClosed');
-eyesClosedFile = sprintf('%s/%s.sqd', dataDir, eyesClosedBase); 
-eyesClosedFileBI = sprintf('%s/%s_bi.sqd', dataDir, eyesClosedBase); 
+% eyesClosedBase = sessionDirToFileBase(sessionDir, 'EyesClosed');
+% eyesClosedFile = sprintf('%s/%s.sqd', dataDir, eyesClosedBase); 
+% eyesClosedFileBI = sprintf('%s/%s_bi.sqd', dataDir, eyesClosedBase); 
 
 %% make directories
 
@@ -50,40 +64,76 @@ end
 if ~exist(matDir,'dir')
     mkdir(matDir)
 end
+if ~exist(prepDir,'dir')
+    mkdir(prepDir)
+end
 
 %% read data
 
+% read preprocessed sqd, prepare save to prep_data and data matrix 
 if readData
-    data = meg_getData(filename,p); % time x channels x trials
-    % save data
-    save(sprintf('%s/data.mat',matDir),'data')
+    [prep_data, data] = meg_getData(filename,p); % time x channels x trials
+    save (sprintf('%s/%s_%s_data.mat',matDir,fileBase,analStr),'data', '-v7.3')
+    save (sprintf('%s/%s_%s_prep_data.mat',prepDir,fileBase,analStr),'prep_data', '-v7.3')
 end
 
 % if data already saved, then load data
 if loadData
-    load(sprintf('%s/data.mat',matDir),'data')
+    load(sprintf('%s/%s_%s_data.mat',matDir,fileBase,analStr),'data')
 end
 
 %% reject trials
 
-data = meg_rejectTrials(data,dataDir); % NaN rejected trials
+data = meg_rejectTrials(data,dataDir); % NaN manually rejected trials
+
+%% threshold channel selector 
+% if selectChannels
+%     meg_selectChannels(sessionDir,data); 
+% elseif loadSelectedChannels 
+%     load(sprintf('%s/T.mat',matDir),'T');  
+%     selectedChannels = T.passCh; 
+% end
+
+if selectChannels
+    figPromAvg = sprintf('%spromAvg',figDir);
+    if ~exist(figPromAvg,'dir')
+        mkdir(figPromAvg)
+    end
+    [pkfH, pkfigNames, Pk] = kt_peakstest(sessionDir,data);
+    rd_saveAllFigs(pkfH, pkfigNames, sessionDir, figPromAvg, [])
+    save(sprintf('%s/Pk_avgProm.mat',matDir),'Pk')
+    
+    close all
+    selectedChannels = Pk.passCh';
+end
+
+if loadSelectedChannels
+    chFile = sprintf('%s/Pk_avgProm.mat',matDir); 
+    load(chFile)
+    selectedChannels = Pk.passCh';
+    channelDir = Pk.promDir(selectedChannels); % positive or negative peak 
+end
+
+%% data direction 
+% flip based on peak prominence direction 
+data= data.*Pk.promDir'; 
 
 %% peak channel selector
 
 % try peak channels 
-[fHpeak,figNamesPk] = meg_selectChannels(sessionDir, data); 
-figDirPeak = sprintf('%speak',figDir);
-if ~exist(figDirPeak,'dir')
-    mkdir(figDirPeak)
-end
-rd_saveAllFigs(fHpeak, figNamesPk, sessionDir, figDirPeak, [])
-close all
+% [fHpeak,figNamesPk] = meg_selectChannels(sessionDir, data); 
+% figDirPeak = sprintf('%speak',figDir);
+% if ~exist(figDirPeak,'dir')
+%     mkdir(figDirPeak)
+% end
+% rd_saveAllFigs(fHpeak, figNamesPk, sessionDir, figDirPeak, [])
+% close all
 
 %% try alpha channel selector 
 % C = meg_selectChannels(sessionDir); 
 
 % selectedChannels = C.chSortAlpha(1:5)'; 
-% 
+
 % figDirAlpha = sprintf('%sAlphaTop5',figDir);
 % if ~exist(figDirAlpha,'dir')
 %     mkdir(figDirAlpha)
@@ -91,27 +141,56 @@ close all
 
 %% slice data by condition (here cue condition) 
 
-% behav = meg_behavior(behav); 
-% conds = behav.responseData_all; % trial x condition 
-% cond = behav.cuedTarget; % attention condition 
-% 
-% condNames = {'cueCond'}; 
-% levelNames = {{'other','cueT1','cueT2'}}; 
-% 
-% D = meg_slicer(data, cond, condNames, levelNames); 
+behav = meg_behavior(behav); 
+conds = behav.responseData_all; % trial x condition 
+cond = behav.cuedTarget; % attention condition 
+
+condNames = {'cueCond'}; 
+
+switch exptShortName 
+    case 'TA2'
+        levelNames = {{'other','cueT1','cueT2','neutral'}}; % levelNames = {{'neutral','cueT1','cueT2','other'}}; 
+    case 'TANoise'
+        levelNames = {{'other','cueT1','cueT2'}};
+end
+cond(isnan(cond))=0; 
+
+D = meg_slicer(data, cond, condNames, levelNames); 
 % D.cueT2subcueT1 = D.cueT2-D.cueT1; 
+
+field = 'other';
+D = rmfield(D,field);
 
 %% ERF analyses
 
-% [fH1,figNames1] = meg_plotERF(D,selectedChannels); 
-% rd_saveAllFigs(fH1, figNames1, sessionDir, figDirAlpha, [])
-% close all
+if plotERF
+    [fH1,figNames1] = meg_plotERF(D,p,selectedChannels);
+    
+    ERFDir = sprintf('%spromAvg/ERF',figDir);
+    if ~exist(ERFDir,'dir')
+        mkdir(ERFDir)
+    end
+    
+    rd_saveAllFigs(fH1, figNames1, sessionDir, ERFDir, [])
+    close all
+end
 
 %% TF analyses
 
-% [A,fH2,figNames2] = meg_plotTF(D,selectedChannels); 
-% rd_saveAllFigs(fH2, figNames2, sessionDir, figDirAlpha, [])
-% close all
+if plotTF
+    [TF,fH2,figNames2] = meg_plotTF(D,p,selectedChannels);
+    
+    thresholdFigTFDir = sprintf('%spromAvg/TF',figDir);
+    if ~exist(thresholdFigTFDir,'dir')
+        mkdir(thresholdFigTFDir)
+    end
+    
+    rd_saveAllFigs(fH2, figNames2, sessionDir, thresholdFigTFDir, [])
+    if saveTF
+        save(sprintf('%s/TF.mat',matDir),'TF','-v7.3')  
+    end
+    close all
+end
 
 %% eyes closed alpha analyses 
 
