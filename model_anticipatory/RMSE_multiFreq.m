59bb773df3a01af0bff9639ee4783b1412d27122
karentian <9136901+karentian@users.noreply.github.com>
2023-10-25 11:16:40 -0400

    move model to independent repo

--- model_anticipatory/RMSE_multiFreq.m
+++
@@ -1,8 +0,0 @@
-
-%% Calculate r^2
-% r^2 = variance explained by the model / total variance 
-% residuals: distance from regression line to each data point 
-% yhat - y 
-% square each residual and sum 
-
-%% Compare RMSE across multiple frequencies  
--- model_anticipatory/meg_rsquared.m
+++
@@ -1,9 +0,0 @@
-function rsq = meg_rsquared(ssr,sst)
-% Calculate r-squared 
-% Inputs:
-%   ssr: sum squared regression, scaler 
-%   sst: total sum of squares, (number of samples), scaler
-% Ouputs 
-%   rsq: r-squared, scaler 
-
-rsq = 1 - ssr/sst; 
