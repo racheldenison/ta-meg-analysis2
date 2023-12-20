e8e346f987b6bfaabf798a7ec8d8b10c17974c54
Karen Tian <karenjtian@gmail.com>
2023-12-20 07:41:35 -0500

    update sessions to subject averager to include 2d input

--- main/meg_sessions2subjects.m
+++ main/meg_sessions2subjects.m
@@ -2,14 +2,19 @@ function subjectData = meg_sessions2subjects(sessionData)
 % function subjectData = meg_sessions2subjects(sessionData)
 % takes 1 x 20 (session) data and averages to 1 x 10 (subject) level 
 
-nSessions = size(sessionData,2); 
+dims = size(sessionData); 
+nSessions = dims(2); 
+if numel(dims) >2 
+    error('Check size of input. Should be val x 20 sessions.')
+end
 nSessionsPerSubject = 2; % number of sessions per subject 
 
-sCount = 1; 
+sCount = 1;
 for i = 1:nSessionsPerSubject:nSessions
-    clear sVal
-    sVal = sessionData(i:i+nSessionsPerSubject-1);
-    sVal = mean(sVal);
-    subjectData(sCount) = sVal; 
-    sCount=sCount+1; 
+    clear val
+        sVal = sessionData(:,i:i+nSessionsPerSubject-1);
+        sVal = mean(sVal,2,'omitnan');
+        subjectData(:,sCount) = sVal;
+    sCount=sCount+1;
 end
+
